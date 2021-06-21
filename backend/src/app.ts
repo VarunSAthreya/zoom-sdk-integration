a704c60863544a3cbe2e306769e93336ef4d8868
Varun S Athreya <varun.sathreya@gmail.com>
2021-06-21 23:14:12 +0530

    Fixed port and script

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
-const port: number = 3000;
+const port: number = 8000;
 app.get("/", (req: Request, res: Response) => {
     res.send("HomePage");
 });
