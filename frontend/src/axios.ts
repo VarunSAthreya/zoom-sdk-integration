e2430fabd1116d2c4eef87be0061fb75aaea9b3b
Varun S Athreya <varun.sathreya@gmail.com>
2021-06-24 23:57:19 +0530

    Added axios config

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,7 +1,7 @@
 import Axios, { AxiosInstance } from "axios";
 
 const axios: AxiosInstance = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+    baseURL: "http://localhost:8000",
     withCredentials: true,
 });
 
